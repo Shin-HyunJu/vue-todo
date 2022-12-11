d73fb1abc28e1c3d3cc2d4d5e5aa5f2e27bea88f
Shin-HyunJu <afww@daum.net>
2022-12-11 15:49:35 +0900

    console 제거

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex); //use는 Vue의 plugin, vue의 모든 기능에 vuex를 추가 하고 싶을 때 작성
+Vue.use(Vuex); //use는 Vue의 plugin, vue의 모든 기능(전역)에 vuex를 추가 하고 싶을 때 작성
 
 const storage = {
   fetch() { //created 대신
     const arr = [];
     if(localStorage.length>0) {
       for(let i=0; i<localStorage.length; i++){
-        //console.log(localStorage.key(i));
         if(localStorage.key(i) !== ''){
           //localStorage.getItem(localStorage.key(i)); //value가져옴
-          console.log(JSON.parse(localStorage.getItem(localStorage.key(i))));
           arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
         }
       }
